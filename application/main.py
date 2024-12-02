d4db7b7d5e4c5568e6ded28e48beb9de597ea0c2
Harshal Mahajan <Harshal.Mahajan@esds.co.in>
2024-12-02 17:45:22 +0530

    changes after modification in main.py file

--- application/main.py
+++ application/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI,HTTPException
 from application.crud import crud_router
 from application.routes.items_crud import item_router
 from application.config import root_env
+
 # from models.item import Base
 
 DB_URL = root_env.env_get("DB_URL")
@@ -12,3 +13,4 @@ app = FastAPI()
 
 app.include_router(crud_router, prefix="/item/crud", tags=["crud"])
 app.include_router(item_router,prefix="/crud",tags=["Item"])
+
